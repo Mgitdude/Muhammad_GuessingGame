0a22572c2ead0f6b3ac103774754bc7e877ad860
MUSgitudev <72415650+MUSgitudev@users.noreply.github.com>
2020-10-23 09:37:45 -0400

    building

---
+++ Muhammad_GuessingGame/view/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Muhammad_GuessingGame
+//
+//  Created by MUS on 2020-10-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Int? = nil
+    @State private var attempts: Int? = nil
+    @State private var guessed_num: String = UserDefaults.standard.string(forKey: "guessed_num") ?? ""
+    //@State private var temp_num: String? = ;
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+           /* NavigationLink(destination: ScoreView(), tag: 1, selection: $selection) {}*/
+                Text("Guessing Game")
+                    .foregroundColor(Color.blue)
+                
+                Text("You have to guess a number between 1 to 25. You have 5 attempts. Can you guess the number correctly?")
+                
+                TextField("Enter your guessed number", text: self.$guessed_num)
+                
+                Text("You have to guess a number between 1 to 25. You have 5 attempts. Can you guess the number correctly?")
+
+          }
+            .navigationBarTitle(Text("Muhammad_GuessingGame"), displayMode: .inline)
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        self.selection = 1
+                                    }) {
+                                        Text("Score")
+                                    }
+                           
+                        )
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
